24d0bdcbac4b9a15fb5350ff96a3a3cef1feda13
Minh Hoang <mhoang.0128@gmail.com>
2019-12-18 00:18:06 -0800

    first commit

---
+++ README.txt
@@ -0,0 +1,23 @@
+Minh Hoang
+12/17/2019
+Language: Python 3
+Instruction Link: https://www.youtube.com/watch?v=7ovFudqFB0Q
+
+Purpose: The program will create a bot that automatically go on Twitter with Firefox web browser, then it will input your username and password and will sign in for you. Next, it will find the hashtag you are trying to get and will automatically find and like all relevant status
+There are 2 files:
+    app.py: The bot will go on 'tuilatrai123' account that I created for this project and start finding '#webdevelopment' and like all of the statuses that it can find
+    app01.py:
+        inputs from user:
+            username
+            password
+            hashtag you want to like:
+        output: the bot will auto run until it is told to stop.
+Requirement:
+    1/Download package selenium and Firefox web browser
+        _From command line: pip install selenium
+    2/ Download geckodriver and put it at user/_your_name_/local/Python
+
+What I learn:
+_How to find element in HTML of Twitter
+_How to control the JavaScript of Twitter by Python
+_How to create a bot to automatically go online and like stuff for you.
---
+++ app01.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class TwitterBot:
+    def __init__(self, username, password):  # remember to not put your password on github
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(2)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    def like_tweet(self, hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q=' + hashtag + '&src=typed_query')
+        time.sleep(3)
+        for i in range(1,3):
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')  # scroll down for us
+            time.sleep(2)
+
+            tweetLinks = [i.get_attribute('href')
+                          for i in bot.find_elements_by_xpath("//a[@dir='auto']")]  # looking for all the element where they have an attribute dir=auto
+            filteredLinks = list(filter(lambda x: 'status' in x, tweetLinks))  # once I have all the hrefs data then I can filter them out to store only the ones with the string "status" in it
+
+            print(filteredLinks)
+            for link in filteredLinks:
+                bot.get(link)
+                time.sleep(3)
+                try:
+                    bot.find_element_by_xpath("//div[@data-testid='like']").click()
+                    time.sleep(10)
+                except Exception as ex:
+                    time.sleep(10)
+username = input('username: ')
+password = input('password: ')
+hashtag = input('hashtag you want to like: ')
+minh = TwitterBot(username,password)
+minh.login()
+minh.like_tweet(hashtag)
